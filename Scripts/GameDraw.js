d4ab9bc4ee941c6e35c2dd5de4dda51afa50ef6d
Boray <boray@abv.bg>
2015-07-23 12:02:04 +0300

    Start with a rough idea.

---
+++ Scripts/GameDraw.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="kinetic-v4.4.3.js" />
+
+var GameDraw = ( function () {           
+    
+    var stage = new Kinetic.Stage( {
+        container: 'kinetic-container',
+        width: 800,
+        height: 600,
+    } );
+
+    var circle = new Kinetic.Circle( {
+        x: 200,
+        y: 300,
+        radius: 100,
+        fill: 'black',
+    } );
+
+    var layer = new Kinetic.Layer();
+
+    layer.add(circle);
+    stage.add( layer );
+
+    function background() {
+        
+        layer.add( circle );
+        stage.add( layer );
+    };
+
+    return {
+        background: background,
+    }
+}() );
---
+++ Scripts/GameObjects.js
@@ -0,0 +1,122 @@
+﻿var GameObjects = ( function () {
+    var Board,
+        Piece,
+        Player,
+        BoardField;
+
+    BoardField = ( function () {
+        var boardField = Object.create( [] );
+
+        Object.defineProperty( boardField, 'init', {
+            value: function () {
+                
+                return this;
+            }
+        } );
+
+        Object.defineProperty( boardField, 'add', {
+            value: function (piece) {
+                this.push( piece );
+
+                return this;
+            }
+        } );       
+
+        return boardField;
+    }() );
+
+    Board = ( function () {
+        var board = Object.create( [] );        
+        var boardLength = 24;
+
+        Object.defineProperty( board, 'init', {
+            value: function (width, height) {
+                var i;
+
+                this.width = width;
+                this.height = height;
+
+                for ( i = 0; i < boardLength; i += 1 ) {
+                    board.push( Object.create(BoardField.init()) );
+                }
+
+                return this;
+            }
+        } );
+
+        Object.defineProperty( board, 'getPiecePosition', {
+            value: function (piece) {
+                
+
+               
+
+                return this;
+            }
+        } );
+
+        return board;
+    }() );
+
+    Piece = ( function () {
+        var piece = Object.create( {} );
+        var radiusSize = 24;
+
+        Object.defineProperty( piece, 'init', {
+            value: function ( x, y, color ) {
+                this.x = x;
+                this.y = y;
+                this.color = color;
+                this._radius = radiusSize;
+
+                return this;
+            }
+        } );
+
+        Object.defineProperty( piece, 'x', {
+            get: function () {
+                return this._x;
+            },
+            set: function ( value ) {
+                this._x = value;
+
+                return this;
+            }
+        } );
+
+        Object.defineProperty( piece, 'y', {
+            get: function () {
+                return this._y;
+            },
+            set: function ( value ) {
+                this._y = value;
+
+                return this;
+            }
+        } );
+
+        Object.defineProperty( piece, 'color', {
+            get: function () {
+                return this._color;
+            },
+            set: function ( value ) {
+                this._color = value;
+
+                return this;
+            }
+        } );
+
+        Object.defineProperty( piece, 'radius', {
+            get: function () {
+                return this._radius;
+            }
+        } );
+
+        return piece;
+    }() );
+
+    return {
+        Board: Board,
+        Piece: Piece
+    }
+
+}() )
